2c31026cd4d5f211abf7f3c69ee6f7a80f5fbc1a
Bob Roberts <broberts_az@yahoo.com>
2012-07-18 15:03:28 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoSite::Application.config.secret_token = '14c083e754d0ecc4405d388c758a172afe34365de1d1ee4e1d414ba2eedbc49dec1d8c575ca0f802a01feac9c2d1cb601fbe4bdcc311687c7935d5aa34731c4f'
