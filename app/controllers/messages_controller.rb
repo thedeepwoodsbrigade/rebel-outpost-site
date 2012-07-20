d5cafeb56549ebe8ce3c1d8a11d3d926391a8f93
Bob Roberts <broberts_az@yahoo.com>
2012-07-20 16:39:14 -0700

    setting up email still

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,10 +7,10 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(params[:message])
     if @message.valid?
-      # TODO send message here
-      redirect_to root_url, notice: "Message sent! Thank you for contacting us."
+      NotificationsMailer.new_message(@message).deliver
+      redirect_to(root_path, :notice => "Message sent! Thank you for contacting us.")
     else
-      render "new"
+      flash.now.alert = "Please fill all fields."
     end
   end
 
---
+++ app/views/notifications_mailer/new_message.text.rb
@@ -0,0 +1,13 @@
+First Name: <%= @message.first_name %>
+
+Last Name: <%= @message.last_name %>
+
+Email: <%= @message.email %>
+
+Phone: <%= @message.phone %>
+
+Company: <%= @message.company %>
+
+Service Wanted: <%= @message.service_wanted %>
+
+Comments: <%= @message.comment %>
